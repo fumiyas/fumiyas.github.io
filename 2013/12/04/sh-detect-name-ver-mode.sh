d1ec4791b5934d57547f464e59703d19bac14194
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2013-12-04 00:37:38 +0900

    sh-detect-name-ver-mode.sh: New for blog

---
+++ 2013/12/04/sh-detect-name-ver-mode.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# or
+#!/bin/bash
+#!/bin/ksh
+#!/bin/zsh
+
+if [ -n "${BASH_VERSION-}" ]; then
+  sh_name='bash'
+  sh_ver_major="${BASH_VERSINFO[0]}"
+  sh_ver_minor="${BASH_VERSINFO[1]}"
+  sh_ver_micro="${BASH_VERSINFO[2]}"
+  sh_ver="$sh_ver_major.$sh_ver_minor.$sh_ver_micro"
+  sh_mode="${BASH##*/}"
+elif [ -n "${ZSH_VERSION-}" ]; then
+  sh_name="zsh"
+  sh_ver="$ZSH_VERSION"
+  sh_mode=$(emulate)
+  sh_ver_major="${ZSH_VERSION%%.*}"
+  sh_ver_minor="${ZSH_VERSION%.*}"
+  sh_ver_minor="${sh_ver_minor#*.}"
+  sh_ver_micro="${ZSH_VERSION##*.}"
+elif [ -n "${RANDOM-}" ]; then
+  sh_name="ksh"
+  if PATH= type builtin >/dev/null 2>&1; then
+    sh_ver="93"
+  else
+    sh_ver="88"
+  fi
+  sh_ver_marjo="$sh_ver"
+else
+  sh_name="sh"
+fi
+
+echo "Name: $sh_name"
+echo "Version: ${sh_ver--}"
+[ -n "$sh_ver_major" ] && echo "  Major: $sh_ver_major"
+[ -n "$sh_ver_minor" ] && echo "  Minor: $sh_ver_minor"
+[ -n "$sh_ver_micro" ] && echo "  Micro: $sh_ver_micro"
+echo "Mode: ${sh_mode--}"
+
