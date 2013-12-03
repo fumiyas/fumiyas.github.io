dd239ce98f8eb7f1d20003142826a5f6d480a7a8
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2013-12-04 01:23:17 +0900

    Blog: シェルの種類とバージョンの検出 - 拡張 POSIX シェルスクリプト Advent Calendar 2013

--- 2013/12/04/sh-detect-name-ver-mode.sh
+++ 2013/12/04/sh-detect-name-ver-mode.sh
@@ -10,6 +10,7 @@ if [ -n "${BASH_VERSION-}" ]; then
   sh_ver_minor="${BASH_VERSINFO[1]}"
   sh_ver_micro="${BASH_VERSINFO[2]}"
   sh_ver="$sh_ver_major.$sh_ver_minor.$sh_ver_micro"
+  sh_ver_number=$(printf '%d%03d%03d' "$sh_ver_major" "$sh_ver_minor" "$sh_ver_micro")
   sh_mode="${BASH##*/}"
 elif [ -n "${ZSH_VERSION-}" ]; then
   sh_name="zsh"
@@ -19,6 +20,7 @@ elif [ -n "${ZSH_VERSION-}" ]; then
   sh_ver_minor="${ZSH_VERSION%.*}"
   sh_ver_minor="${sh_ver_minor#*.}"
   sh_ver_micro="${ZSH_VERSION##*.}"
+  sh_ver_number=$(printf '%d%03d%03d' "$sh_ver_major" "$sh_ver_minor" "$sh_ver_micro")
 elif [ -n "${RANDOM-}" ]; then
   sh_name="ksh"
   if PATH= type builtin >/dev/null 2>&1; then
@@ -27,12 +29,14 @@ elif [ -n "${RANDOM-}" ]; then
     sh_ver="88"
   fi
   sh_ver_marjo="$sh_ver"
+  sh_ver_int="$sh_ver"
 else
   sh_name="sh"
 fi
 
 echo "Name: $sh_name"
 echo "Version: ${sh_ver--}"
+[ -n "$sh_ver_number" ] && echo "  Number: $sh_ver_number"
 [ -n "$sh_ver_major" ] && echo "  Major: $sh_ver_major"
 [ -n "$sh_ver_minor" ] && echo "  Minor: $sh_ver_minor"
 [ -n "$sh_ver_micro" ] && echo "  Micro: $sh_ver_micro"
