397cd7e24ce351cd19cb945aafdc278ebe03bb74
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2019-04-10 12:19:23 +0900

    Update nomember-buggy.bash

--- 2013/12/10/nomember-buggy.bash
+++ 2013/12/10/nomember-buggy.bash
@@ -3,10 +3,10 @@
 typeset -a groups_wo_member
 
 getent group \
-  |sed -n 's#:.*[^:]$##p' \
-  |while IFS= read -r group; do
-    groups_wo_member+=("$group")
-  done \
+|sed -n 's#:.*[^:]$##p' \
+|while IFS= read -r group; do
+  groups_wo_member+=("$group")
+done \
 ;
 
 echo "${#groups_wo_member[*]}"
